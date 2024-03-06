b1b76ddd5578583b18d51ec5458ba02f212a3bff
Ronlivgit <ron.livne58@gmail.com>
2024-03-06 10:25:55 +0200

    a

--- models/order.model.js
+++ models/order.model.js
@@ -1,16 +1,18 @@
 //! Model will include the Schema 
 const mongoose = require("mongoose")
 
-
+//! OrderStatus = awaiting, confirmed, shipped, arrived , confirmed
 const orderSchema = new mongoose.Schema({
-    email : {type : String , required : true , unique: true },
+    email : {type : String , required : true },
     fullName : {type : String , required : true },
-    password : {type : String , required : true },
+    password : {type : String },
     role: {type : String , default : "user"},
     birthdate : {type : Date , required : true},
     userId : {type : mongoose.Types.ObjectId , ref: 'User' } ,
     address: {type : String } ,
-    paymentOptions : {type : Array}
+    paymentOptions : {type : Array},
+    products : {type : Array},
+    orderStatus : {type : String , default : "awaiting"}
 })
 
 const Order = mongoose.model("Orders" , orderSchema)
